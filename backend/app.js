7e78fd295bba0539718e6ae98e66412f7fc0f85d
TuanKietIT <ngokiet688@gmail.com>
2022-11-08 19:33:30 +0700

    make it better 1

--- backend/app.js
+++ backend/app.js
@@ -35,10 +35,10 @@ app.use("/api/v2",payment);
 
 app.use("/api/v2",cart);
 
-app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.use(express.static(path.join("../frontend/build")));
 
 app.get("*",(req,res) =>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
+    res.sendFile(path.resolve("../frontend/build/index.html"));
 })
 
 // it's for errorHandeling
