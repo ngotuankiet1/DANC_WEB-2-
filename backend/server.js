83228a059acef0a7e0aa16c30d5b56a186b5c439
TuanKietIT <ngokiet688@gmail.com>
2022-11-08 20:00:50 +0700

    4

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,9 @@ cloudinary.config({
 })
 
 // create server
-const server = app.listen(process.env.PORT,() =>{
-    console.log(`Server is working on http://localhost:${process.env.PORT}`)
+const port = process.env.PORT || 5000;
+const server = app.listen(port,() =>{
+    console.log(`Server is working on http://localhost:${port}`)
 })
 
 
