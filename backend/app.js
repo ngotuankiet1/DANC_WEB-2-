d49a60a7b62d0e6047f98e06adfa1c891f5c0a8e
TuanKietIT <ngokiet688@gmail.com>
2022-11-08 21:57:29 +0700

    9

--- backend/app.js
+++ backend/app.js
@@ -35,11 +35,11 @@ app.use("/api/v2",payment);
 
 app.use("/api/v2",cart);
 
-app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.use(express.static("frontend/build"));
 
-app.get("*",(req,res) =>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 
 // it's for errorHandeling
 app.use(ErrorHandler);
