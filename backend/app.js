55c1023130f3c0b0ee345f861d588b052baf15b5
TuanKietIT <ngokiet688@gmail.com>
2022-11-08 18:41:43 +0700

    5.2

--- backend/app.js
+++ backend/app.js
@@ -8,21 +8,15 @@ const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(fileUpload({ useTempFiles: true }));
+app.use(fileUpload({useTempFiles: true}));
 
 // config
-if (process.env.NODE_ENV === "PRODUCTION") {
-  // require("dotenv").config({
-  //     path:"backend/config/.env"
-  // })
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-  });
-}
+if(process.env.NODE_ENV!=="PRODUCTION"){
+    require("dotenv").config({
+        path:"backend/config/.env"
+    })}
 
 // Route imports
 const product = require("./routes/ProductRoute");
@@ -31,17 +25,23 @@ const order = require("./routes/OrderRoute");
 const payment = require("./routes/PaymentRoute");
 const cart = require("./routes/WishListRoute");
 
-app.use("/api/v2", product);
+app.use("/api/v2",product);
 
-app.use("/api/v2", user);
+app.use("/api/v2",user);
 
-app.use("/api/v2", order);
+app.use("/api/v2",order);
 
-app.use("/api/v2", payment);
+app.use("/api/v2",payment);
 
-app.use("/api/v2", cart);
+app.use("/api/v2",cart);
+
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+
+app.get("*",(req,res) =>{
+    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
+})
 
 // it's for errorHandeling
 app.use(ErrorHandler);
 
-module.exports = app;
+module.exports = app
