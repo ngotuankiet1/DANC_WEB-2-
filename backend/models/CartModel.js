db52d8864882835f2356c87467b2b8d0c10a58d8
TuanKietIT <ngokiet688@gmail.com>
2022-11-09 22:41:58 +0700

    make it better

--- backend/models/CartModel.js
+++ backend/models/CartModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
-  productName: {
+  product: {
     type: String,
     required: [true, "Please enter your product name"],
   },
